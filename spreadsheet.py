7af355fdeb71b1881de420780def5500aa57b58c
chan fulmer <chandlerfulmer@gmail.com>
2019-10-16 21:57:10 -0400

    SpreadsheetProgram

---
+++ spreadsheet.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+# Copyright (c) 2014 Adafruit Industries
+# Author: Tony DiCola
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+import json
+import sys
+import time
+import datetime
+
+import Adafruit_DHT
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+DHT_TYPE = Adafruit_DHT.DHT22
+
+DHT_PIN = 7
+
+GDOCS_OAUTH_JSON        = 'client_secret_424796253504-7lgtqeujnqu17nbbgqpdo3g39ogtjv55.apps.googleusercontent.com.json'
+
+GDOCS_SPREADSHEET_NAME  = 'AeroponicsData'
+
+FREQUENCY_SECONDS       = 60
+
+def login_open_sheet(oauth_key_file, spreadsheet):
+    """Connect to Google Docs spreadsheet and return the first worksheet."""
+    try:
+        scope =  ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(oauth_key_file, scope)
+        gc = gspread.authorize(credentials)
+        worksheet = gc.open(spreadsheet).sheet1
+        return worksheet
+    except Exception as ex:
+        print('Unable to login and get spreadsheet.  Check OAuth credentials, spreadsheet name, and make sure spreadsheet is shared to the client_email address in the OAuth .json file!')
+        print('Google sheet login failed with error:', ex)
+        sys.exit(1)
+
+
+print('Logging sensor measurements to {0} every {1} seconds.'.format(GDOCS_SPREADSHEET_NAME, FREQUENCY_SECONDS))
+print('Press Ctrl-C to quit.')
+worksheet = None
+while True:
+    # Login if necessary.
+    if worksheet is None:
+        worksheet = login_open_sheet(GDOCS_OAUTH_JSON, GDOCS_SPREADSHEET_NAME)
+
+    # Attempt to get sensor reading.
+    humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
+
+    # Skip to the next reading if a valid measurement couldn't be taken.
+    if humidity is None or temp is None:
+        time.sleep(2)
+        continue
+
+    print('Temperature: {0:0.1f} C'.format(temp))
+    print('Humidity:    {0:0.1f} %'.format(humidity))
+
+    # Append the data in the spreadsheet, including a timestamp
+    try:
+        worksheet.append_row((datetime.datetime.now().isoformat(), temp, humidity))
+    except:
+        # Error appending data, bad credentials
+        # Null out the sheet for fresh restart
+        print('Append error, logging in again')
+        worksheet = None
+        time.sleep(FREQUENCY_SECONDS)
+        continue
+
+    # Wait 60 seconds before continuing
+    print('Wrote a row to {0}'.format(GDOCS_SPREADSHEET_NAME))
+    time.sleep(FREQUENCY_SECONDS)
