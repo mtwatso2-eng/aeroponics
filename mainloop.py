98bd4fc7d311c8a3847392e59c97f3c9d00aa107
chan fulmer <chandlerfulmer@gmail.com>
2019-12-02 16:17:10 -0500

    wait

--- mainloop.py
+++ mainloop.py
@@ -65,10 +65,11 @@ while True:
     humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
     ads = ADS.ADS1115(i2c)
     chan = AnalogIn(ads, ADS.P2)
+    sleep(5)
     float(humidity)
     float(temp)
     float(chan.value)
-    
+
     HUM.append(humidity)
     TEM.append(temp)
     LIGHT.append(chan.value)
