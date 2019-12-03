bb50732d276a0cdceb4d62c3da4530ecd5b12f2a
chan fulmer <chandlerfulmer@gmail.com>
2019-12-03 16:23:41 -0500

    one fix

--- mainloop.py
+++ mainloop.py
@@ -86,7 +86,7 @@ while True:
         HUM.append(humidity)
         TEMP.append(temperature)
         #LIGHT.append(chan.value)
-    print(len(HUM), len(TEMP), len(LIGHT))
+    print(len(HUM), len(TEMP)) #len(LIGHT))
     HUMsum = sum(HUM)
     TEMPsum = sum(TEMP)
     #LIGHTsum = sum(LIGHT)
