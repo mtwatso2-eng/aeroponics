b4ac7633428979337cff5d09939341cf265edc5e
chan fulmer <chandlerfulmer@gmail.com>
2019-12-09 13:58:51 -0500

    comment

--- spreadsheet.py
+++ spreadsheet.py
@@ -31,7 +31,7 @@ import socket
 
 # Configure ADC connections
 # Try to create an I2C device
-i2c = busio.I2C(board.SCL, board.SDA)
+#i2c = busio.I2C(board.SCL, board.SDA)
 # Try to create an SPI device
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
@@ -71,8 +71,8 @@ while True:
 
     # Attempt to get sensor reading.
     humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
-    ads = ADS.ADS1115(i2c)
-    chan = AnalogIn(ads, ADS.P2)
+    #ads = ADS.ADS1115(i2c)
+    #chan = AnalogIn(ads, ADS.P2)
 
     #Fetch IP address
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -90,7 +90,7 @@ while True:
 
     print(temp)
     print(humidity)
-    print(chan.value, chan.voltage)
+    #print(chan.value, chan.voltage)
 
     # Append the data in the spreadsheet, including a timestamp
     try:
