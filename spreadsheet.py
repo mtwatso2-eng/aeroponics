800b9f96b130df179b9eca7e7895b94735eb7051
chan fulmer <chandlerfulmer@gmail.com>
2019-12-09 13:54:19 -0500

    dfsa

--- spreadsheet.py
+++ spreadsheet.py
@@ -31,7 +31,7 @@ import socket
 
 # Configure ADC connections
 # Try to create an I2C device
-#i2c = busio.I2C(board.SCL, board.SDA)
+i2c = busio.I2C(board.SCL, board.SDA)
 # Try to create an SPI device
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
@@ -71,15 +71,15 @@ while True:
 
     # Attempt to get sensor reading.
     humidity, temp = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
-#    ads = ADS.ADS1115(i2c)
-#    chan = AnalogIn(ads, ADS.P2)
+    ads = ADS.ADS1115(i2c)
+    chan = AnalogIn(ads, ADS.P2)
 
-    #Fetch IP address
-    #s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    #s.connect(("8.8.8.8", 80))
-    #ip = (s.getsockname()[0])
-    #print(ip)
-    #s.close()
+    Fetch IP address
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = (s.getsockname()[0])
+    print(ip)
+    s.close()
 
     # Skip to the next reading if a valid measurement couldn't be taken.
 
@@ -88,9 +88,9 @@ while True:
     else:
         continue
 
-    print('Temperature: {0:0.1f} C'.format(temp))
-    print('Humidity:    {0:0.1f} %'.format(humidity))
-#    print(chan.value, chan.voltage)
+    print(temp)
+    print((humidity)
+    print(chan.value, chan.voltage)
 
     # Append the data in the spreadsheet, including a timestamp
     try:
