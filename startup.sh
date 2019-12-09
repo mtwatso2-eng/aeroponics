e706e36bc7953dcfe5582e581c2fc287e4ca59a9
Sweet Potato <cfulmer@ncsu.edu>
2019-12-09 14:09:27 -0500

    sufjds;fa

--- startup.sh
+++ startup.sh
@@ -2,10 +2,10 @@
 # startup.sh
 # change directories and execute startup programs
 
-sudo python3 spreadsheet.py &
-sudo python3 mainloop.py &
-sleep 1m
-sudo python3 analysis.py &
-sleep 10
-sudo python3 solenoid-cutting.py &
+#sudo python3 spreadsheet.py &
+#sudo python3 mainloop.py &
+#sleep 1m
+#sudo python3 analysis.py &
+#sleep 10
+#sudo python3 solenoid-cutting.py &
 
