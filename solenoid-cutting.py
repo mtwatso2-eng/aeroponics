522415ba46e1c161898c5ce44751b63248ced646
chan fulmer <chandlerfulmer@gmail.com>
2019-12-10 10:54:23 -0500

    fds

--- solenoid-cutting.py
+++ solenoid-cutting.py
@@ -30,21 +30,20 @@ while True:
     file = open("OFF.txt", "r")
     OFF = file.read()
     OFF = int(OFF)
+    #print(OFF)
+    GPIO.setmode(GPIO.BCM)
+    for i in pinList:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, GPIO.HIGH)
     try:
-        #print(OFF)
-        GPIO.setmode(GPIO.BCM)
-        for i in pinList:
-            GPIO.setup(i, GPIO.OUT)
-            GPIO.output(i, GPIO.HIGH)
-        try:
-            GPIO.output(21, GPIO.LOW)
-            #print ("ON")
-            time.sleep(ON);
-            GPIO.output(21, GPIO.HIGH)
-            #print("OFF")
-            time.sleep(OFF)
-            GPIO.cleanup()
-            continue
-        except (KeyboardInterrupt, SystemExit):
-            GPIO.cleanup()
-            break
+        GPIO.output(21, GPIO.LOW)
+        #print ("ON")
+        time.sleep(ON);
+        GPIO.output(21, GPIO.HIGH)
+        #print("OFF")
+        time.sleep(OFF)
+        GPIO.cleanup()
+        continue
+    except (KeyboardInterrupt, SystemExit):
+        GPIO.cleanup()
+        break
