ed3569d5b31a74cdd43ec2931118c8a018929d22
chan fulmer <chandlerfulmer@gmail.com>
2019-12-10 10:32:02 -0500

    relay

--- solenoid-cutting.py
+++ solenoid-cutting.py
@@ -7,7 +7,7 @@ import datetime
 import RPi.GPIO as GPIO
 #from setup import pinList
 import time
-import threading
+
 
 # loop through pins and set mode and state to 'high'
 #Set Relay Pins
@@ -30,19 +30,20 @@ while True:
     file = open("OFF.txt", "r")
     OFF = file.read()
     OFF = int(OFF)
-    #print(OFF)
-    GPIO.setmode(GPIO.BCM)
-    for i in pinList:
-        GPIO.setup(i, GPIO.OUT)
-        GPIO.output(i, GPIO.LOW)
     try:
-        GPIO.output(21, GPIO.HIGH)
-        #print ("ON")
-        time.sleep(ON);
-        GPIO.output(21, GPIO.LOW)
-        #print("OFF")
-        time.sleep(OFF)
-        GPIO.cleanup()
-        continue
-    except (KeyboardInterrupt, SystemExit):
-        break
+        #print(OFF)
+        GPIO.setmode(GPIO.BCM)
+        for i in pinList:
+            GPIO.setup(i, GPIO.OUT)
+            GPIO.output(i, GPIO.HIGH)
+        try:
+            GPIO.output(21, GPIO.LOW)
+            #print ("ON")
+            time.sleep(ON);
+            GPIO.output(21, GPIO.HIGH)
+            #print("OFF")
+            time.sleep(OFF)
+            GPIO.cleanup()
+            continue
+        except (KeyboardInterrupt, SystemExit):
+            break
