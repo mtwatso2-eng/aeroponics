d9e2bd0dd2dfa0a1e4aed1d57ae5a8e9e209edd2
Sweet Potato <cfulmer@ncsu.edu>
2019-12-10 10:16:10 -0500

    fhdjsak

--- ListsTest.py
+++
@@ -1,4 +0,0 @@
-light = [300, 600, 900, 300, 600]
-for num in light
-    avg += num
-print avg
--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # startup.sh
 # change directories and execute startup programs
-
+ cd /home/pi/code/aeroponics
+ sudo python3 spreadsheet.py &
 #sudo python3 spreadsheet.py &
 #sudo python3 mainloop.py &
 #sleep 1m
 #sudo python3 analysis.py &
 #sleep 10
 #sudo python3 solenoid-cutting.py &
-
