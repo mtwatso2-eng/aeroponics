7ee50ac3ebb066c3cbddd6f6e1feb20557c21c05
chan fulmer <chandlerfulmer@gmail.com>
2019-12-10 11:39:46 -0500

    f;kjdsjalk

--- mainloop.py
+++ mainloop.py
@@ -27,6 +27,7 @@ import datetime
 import sys
 import threading
 
+#This is for the Photoresistor
 # Configure ADC connections
 # Try to create an I2C device
 #i2c = busio.I2C(board.SCL, board.SDA)
@@ -34,33 +35,30 @@ import threading
 # Try to create an SPI device
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-print("Connected")
+#print("Connected")
 
 # Set Sensor Type and Pin Number (BCM)
 DHT_TYPE = Adafruit_DHT.DHT22
 DHT_PIN = 23
 
 #Set loop time in seconds
+#how often a measurement will be taken
 FREQUENCY_SECONDS       = 10
 
-#PrintToConsole
-print('Logging sensor measurements to {0} every {1} seconds.')
-print('Press Ctrl-C to quit.')
+#PrintToConsole - Not Necessary
+#print('Logging sensor measurements to {0} every {1} seconds.')
+#print('Press Ctrl-C to quit.')
 
 #setvariables and type    ####This is probably unneccessary
 humidity = 0.0
 temperature = 0.0
 ads = 0.0
-chan = 0.0# Reset GPIO settings
+chan = 0.0
 
-
-
-#create lists ###And this?? Maybe?
+#create lists ###And this also?? Maybe?
 HUM = []
 TEMP = []
 #LIGHT = []
-
-OFF = 300
 
 
 #Loop
@@ -69,14 +67,20 @@ while True:
     humidity, temperature = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
     #ads = ADS.ADS1115(i2c)
     #chan = AnalogIn(ads, ADS.P2)
+
+    #Check for null data and remeasure
     if humidity is None or temperature is None:
         time.sleep(2)
         continue
+    #add value to the end of the list
     HUM.append(humidity)
     TEMP.append(temperature)
     #LIGHT.append(chan.voltage)
+    #check length of list
     count = len(HUM)
+    #amount of values to average
     length = 15
+    #check for correct amount. Removes or adds
     if (count == length):
         continue
     elif (count > length):
@@ -87,19 +91,22 @@ while True:
         HUM.append(humidity)
         TEMP.append(temperature)
         #LIGHT.append(chan.value)
+    #Average the values
     count = len(HUM)
-    print(len(HUM), len(TEMP)) #len(LIGHT))
+    #print(len(HUM), len(TEMP)) #len(LIGHT))
     HUMsum = sum(HUM)
     TEMPsum = sum(TEMP)
     #LIGHTsum = sum(LIGHT)
     HUMavg = HUMsum/count
     TEMPavg = TEMPsum/count
     #LIGHTavg = LIGHTsum/count
-    print('Temperature: {0:0.1f} C'.format(TEMPavg))
-    print('Humidity:    {0:0.1f} %'.format(HUMavg))
+    #print('Temperature: {0:0.1f} C'.format(TEMPavg))
+    #print('Humidity:    {0:0.1f} %'.format(HUMavg))
+    #Clean everything up
     TEMPavg = round(TEMPavg, 3)
     HUMavg = round(HUMavg, 3)
-    print(LIGHT)
+    #print(LIGHT)
+    #Remove first value of the list
     HUM.pop(0)
     TEMP.pop(0)
     #LIGHT.pop(0)
