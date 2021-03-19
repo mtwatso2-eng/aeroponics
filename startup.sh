563ef5b7e4acb42a12ab57461c2db41a6f155d0c
mtwatso2 <mtwatso2@ncsu.edu>
2021-03-19 15:00:17 +0000

    renamed .py files to reflect new roles 	deleted:    mainloop.py 	deleted:    solenoid-cutting.py 	modified:   startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-# startup.sh
 # change directories and execute startup programs
-#cd ~/aeroponics
-sudo python3 mainloop.py &
-sleep 2
-sudo python3 analysis.py &
-sleep 2
-#sudo python3 solenoid-cutting.py
+sudo python3 solenoid-frequency.py &
+sleep 5
+sudo python3 solenoid-control.py &
